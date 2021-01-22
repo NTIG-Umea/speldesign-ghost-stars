bd1b9e1e36c116854918f5f0189ab32fdb7bf284
Alluns <allan.backman@gmail.com>
2021-01-22 10:53:33 +0100

    Hate(tiledLacksDocumentation): Minor fixes to improve looks

--- src/gameConfig.js
+++ src/gameConfig.js
@@ -7,8 +7,8 @@ import PauseScene from "./scenes/Pause";
 
 export default {
   type: Phaser.AUTO,
-  width: 1024,
-  height: 768,
+  width: 800,
+  height: 600,
   title: "Exempel med parcel",
   banner: { text: "white", background: ["#FD7400", "#FFE11A", "#BEDB39", "#1F8A70", "#004358"] },
   scene: [BootScene, MenuScene, PlayScene, EndScene, PauseScene],
--- src/scenes/Pause.js
+++ src/scenes/Pause.js
@@ -8,14 +8,14 @@ export default class PauseScene extends Phaser.Scene {
   create () {
     console.log(this.scene.isSleeping('play'));
 
-    this.add.text(400, 100, 'Pause Menu Test', {
+    this.add.text(400, 200, '- PAUSED -', {
       align: 'center',
       fill: 'white',
       fontFamily: 'sans-serif',
       fontSize: 55
     }).setOrigin(0.5, 0);
 
-    const resumeButton = this.add.text(400, 300, 'RESUME', {
+    const resumeButton = this.add.text(400, 300, '< RESUME >', {
       align: 'center',
       fill: 'white',
       fontFamily: 'sans-serif',
@@ -23,15 +23,5 @@ export default class PauseScene extends Phaser.Scene {
     }).setOrigin(0.5, 0);
     resumeButton.setInteractive();
     resumeButton.on('pointerdown', () => { this.scene.switch('play'); });
-
-    const menuButton = this.add.text(400, 400, 'Return to menu', {
-      align: 'center',
-      fill: 'white',
-      fontFamily: 'sans-serif',
-      fontSize: 45
-    }).setOrigin(0.5, 0);
-    menuButton.setInteractive();
-    menuButton.on('pointerdown', () => { this.scene.switch('menu'); });
-
   }
 }
